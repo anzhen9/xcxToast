d2cf9a93ab1efb696b378daa7aed5a9abc04fc6b
anzhen9 <326346224@qq.com>
2018-09-30 17:27:50 +0800

    master

---
+++ components/toast/toast.js
@@ -0,0 +1,78 @@
+/**
+ * 提醒消息弹窗
+ * @param   style       弹窗样式：1浮动中央，2吸附边缘   默认1
+ * @param   text        提示内容
+ * @param   duration    显示时间  为0则永久显示 单位：毫秒   默认2000
+ * @param   flag        提示类型：success成功消息提示，warning警告消息提示，loading加载消息提示，none不显示   默认none
+ * @param   bgc         自定义背景色    style=2生效，使用css颜色值表示，如：#ff0000，rgb(255,0,0)，rgba(255,0,0,0.5)    默认空
+ * @param   tc          自定义文字色    style=2生效，使用css颜色值表示，如：#ff0000，rgb(255,0,0)，rgba(255,0,0,0.5)    默认空
+ * @param   src         自定义图像    style=1生效
+ * @param   url         跳转链接    style=2生效
+ * @param   position    弹窗位置：top顶部，bottom底部，center中间；   默认center  style=2时center等同top
+ */
+const app = getApp();
+var isPlainObject = obj => {
+  for (var name in obj) {
+    return false;
+  }
+  return true;
+}
+Component({
+  properties: {
+    detail: {
+      type: Object,
+      value: {
+        text: '',
+        flag: 'none',
+        bgc: '',
+        tc: '',
+        src: '',
+        url: '',
+        style: 1,
+        position: 'center',
+        duration: 2000
+      },
+      observer: (newVal, oldVal) => {
+        if (!isPlainObject(newVal)) {
+          let pages = getCurrentPages();
+          let page = pages[pages.length - 1];
+          var duration = typeof newVal.duration == 'number' ? newVal.duration : 2000;
+          if (duration > 0) {
+            setTimeout(() => {
+              page.setData({
+                toast: {}
+              })
+            }, duration);
+          }
+        }
+      }
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    hideToast() {
+      let pages = getCurrentPages();
+      let page = pages[pages.length - 1];
+      page.setData({
+        toast: {}
+      })
+    },
+    turnToPage() {
+      var url = this.data.detail.url;
+      if (url) {
+        var tabBar = '';//tabBar页面,多个页面之间用逗号隔开
+        if (tabBar.indexOf(url) != -1) {
+          wx.switchTab({
+            url: url,
+          })
+          return;
+        }
+        wx.navigateTo({
+          url: url
+        })
+      }
+    }
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,90 @@
+const app = getApp()
+
+Page({
+  data: {
+    toastTemp: {
+      text: '',
+      flag: 'none',
+      bgc: '',
+      tc: '',
+      src: '',
+      url: '',
+      style: 1,
+      position: 'center',
+      duration: 2000
+    },
+    toast:{}
+  },
+  
+  onLoad: function () {
+    
+  },
+  selectStyle(e){
+    var toastTemp = this.data.toastTemp;
+    toastTemp.style = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeText(e){
+    var toastTemp = this.data.toastTemp;
+    toastTemp.text = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeDuration(e){
+    var toastTemp = this.data.toastTemp;
+    toastTemp.duration = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  selectFlag(e){
+    var toastTemp = this.data.toastTemp;
+    toastTemp.flag = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeBgc(e) {
+    var toastTemp = this.data.toastTemp;
+    toastTemp.bgc = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeTc(e) {
+    var toastTemp = this.data.toastTemp;
+    toastTemp.tc = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeSrc(e) {
+    var toastTemp = this.data.toastTemp;
+    toastTemp.src = e.detail.value ? '/images/demo.gif' : '';
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  changeUrl(e) {
+    var toastTemp = this.data.toastTemp;
+    toastTemp.url = e.detail.value ? '/pages/logs/logs' : '';
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  selectPosition(e) {
+    var toastTemp = this.data.toastTemp;
+    toastTemp.position = e.detail.value;
+    this.setData({
+      toastTemp: toastTemp
+    })
+  },
+  showToast(){
+    this.setData({
+      toast: this.data.toastTemp
+    })
+  }
+})
